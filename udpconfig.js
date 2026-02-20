290986b7a992da127ddfe58a976c8729fd92987f
jmcastilla <jmcastilla91@gmail.com>
2026-02-20 15:55:02 -0500

    prueba

--- udpconfig.js
+++ udpconfig.js
@@ -1,6 +1,6 @@
 // udpconfig.js
 // Solo UDP host/port (sin MySQL)
 module.exports = {
-  UDP_HOST: process.env.UDP_HOST || "127.0.0.1",
+  UDP_HOST: process.env.UDP_HOST || "0.0.0.0",
   UDP_PORT: Number(process.env.UDP_PORT || 9007),
 };
